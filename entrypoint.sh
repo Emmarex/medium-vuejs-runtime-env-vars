12996824b44829583ab63426db6927fb017e0226
Tairu Oluwafemi <tairuoluwafemi09@gmail.com>
2020-11-10 02:29:23 +0100

    support for OSX and Linux
    updated entrypoint.sh to add support for OSX and Linux

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,10 @@ JSON_STRING='window.configs = { \
   "VUE_APP_VARIABLE_2":"'"${VUE_APP_VARIABLE_2}"'" \
 }'
 
-sed -i "s@// CONFIGURATIONS_PLACEHOLDER@${JSON_STRING}@" /usr/share/nginx/html/index.html
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  sed -i "" "s@// CONFIGURATIONS_PLACEHOLDER@${JSON_STRING}@" /usr/share/nginx/html/index.html
+else
+  sed -i "s@// CONFIGURATIONS_PLACEHOLDER@${JSON_STRING}@" /usr/share/nginx/html/index.html
+fi
 
 exec "$@"
